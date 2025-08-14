b59e586227a2d737191f2290364db089cd3742ad
Jorge Ballesteros <joba1021@netcracker.com>
2025-08-14 16:42:00 -0500

    initial commit

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_controller import FlaskControllerRegister
 from src.models import Base, engine
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 register_controllers = FlaskControllerRegister(app)
 register_controllers.register_package('src.controllers')
